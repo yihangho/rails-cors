e87a07c39dc927007d22f45c0d0161b399147a45
Yihang Ho <hoyihang5@gmail.com>
2014-08-10 00:13:06 +0800

    Generate Posts controller and return dummy data.

---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,9 @@
+class PostsController < ApplicationController
+  def index
+    render :json => ["post 1", "post 2", "post 3"]
+  end
+
+  def destroy
+    render :json => { :status => "OK" }
+  end
+end
